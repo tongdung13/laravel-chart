3b30e072eb97852230da9d3928dab795ec7417ad
tongdung13 <tongvanthienvu@gmail.com>
2022-07-29 15:34:36 +0700

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -33,7 +33,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous">
     </script>
- <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.6.0/Chart.bundle.js" charset="utf-8"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.6.0/Chart.bundle.js" charset="utf-8"></script>
 
     <script>
         var url = "{{ url('stock/chart') }}";
